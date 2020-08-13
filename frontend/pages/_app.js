cfb5b0262bf028186a22e7360b71244dd531fc2f
lyandeveloperm <tutors3d@gmail.com>
2020-08-13 14:45:02 -0300

    feat: create carousel

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,9 @@
 import "../global.css";
 
 import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
