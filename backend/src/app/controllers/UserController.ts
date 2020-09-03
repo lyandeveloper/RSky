ffb7a16994c0f177238e54840cf1740601dde057
lyandeveloperm <tutors3d@gmail.com>
2020-09-03 16:00:17 -0300

    feat: list users

--- backend/src/app/controllers/UserController.ts
+++ backend/src/app/controllers/UserController.ts
@@ -3,13 +3,20 @@ import { getRepository } from 'typeorm';
 import User from '../entities/User';
 
 class UserController {
-  async createUser(req: Request, res: Response) {
+  async createUser(req: Request, res: Response): Promise<Response> {
     const userData = req.body;
 
     const userRepository = getRepository(User);
     await userRepository.insert(userData);
 
     return res.json(userData);
+  }
+
+  async listUsers(req: Request, res: Response): Promise<Response> {
+    const userRepository = getRepository(User);
+    const users = await userRepository.find();
+
+    return res.json(users);
   }
 }
 
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,5 +4,6 @@ import UserController from './app/controllers/UserController';
 const routes = Router();
 
 routes.post('/user/create', UserController.createUser);
+routes.get('/user/list', UserController.listUsers);
 
 export default routes;
