603886845016933853f3dff4c4d707bbbf1c630d
lyandeveloperm <tutors3d@gmail.com>
2020-10-09 16:12:31 -0300

    feat: Mock files content

--- frontend/pages/explore/index.js
+++ frontend/pages/explore/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import SwiperCore, { Navigation, Pagination, A11y, Autoplay } from 'swiper';
+import estates from '../../services/content';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Header from '../../components/HeaderSecondary';
 import SideBar from '../../components/SideBar';
@@ -23,52 +24,18 @@ function Explore() {
       <main>
         <SideBar />
         <section className="houses_content container">
-          <Estate
-            imgUrl="https://troyhomes.co.uk/wp-content/uploads/2016/11/Mirror-House-4.jpg"
-            price="240.000"
-            description="Casa térrea com sala de estar , sala de jantar , cozinha, 5
-              dormitórios"
-            location="Toronto, Canadá"
-            area="150m2"
-            bathrooms={3}
-            rooms={5}
-          />
-          <Estate
-            imgUrl="https://europetrippers.files.wordpress.com/2013/04/10597113.jpg"
-            location="Las Vegas, Eua"
-            rooms={4}
-            bathrooms={3}
-            description="Uma bela casa com uma linda vista a espera de você."
-            area="120m2"
-            price="180.000"
-          />
-          <Estate
-            imgUrl="https://i.pinimg.com/originals/93/f9/96/93f996eff6750f30d56f6fed11edd368.jpg"
-            location="Califórnia, Eua"
-            description="Casa com garagem, 2 andares e muito mais"
-            rooms={4}
-            bathrooms={2}
-            area="100m2"
-            price="150.000"
-          />
-          <Estate
-            imgUrl="https://www.caandesign.com/wp-content/uploads/2015/07/Al-Rio-de-Janeiro-02-1.jpg"
-            location="Rio de Janeiro, Brasil"
-            description="Casa com uma vista linda de frente ao mar e com piscina"
-            rooms={3}
-            bathrooms={3}
-            area="115m2"
-            price="120.000"
-          />
-          <Estate
-            imgUrl="https://imagens-revista.vivadecora.com.br/uploads/2015/06/Reforma-de-fachada2.jpg"
-            location="São Paulo, Brasil"
-            description="Desfrute morar na maior cidade do Brasil e no melhor lugar"
-            rooms={2}
-            bathrooms={3}
-            area="124m2"
-            price="80.000"
-          />
+          {estates.map((estate) => (
+            <Estate
+              key={estate.id}
+              imgUrl={estate.img}
+              price={estate.price}
+              description={estate.description}
+              location={estate.location}
+              area={estate.area}
+              bathrooms={estate.bathrooms}
+              rooms={estate.rooms}
+            />
+          ))}
         </section>
       </main>
       <style jsx>{`
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import estates from '../services/content';
 import {
   FaArrowRight,
   FaSearch,
@@ -118,66 +119,18 @@ export default function App() {
           </span>
 
           <Swiper spaceBetween={150} slidesPerView={4}>
-            <SwiperSlide>
-              <RecentEstate
-                imgUrl="https://troyhomes.co.uk/wp-content/uploads/2016/11/Mirror-House-4.jpg"
-                name="Palace"
-                location="Toronto, Canadá"
-                rooms={5}
-                bathrooms={3}
-                price="240.000"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <RecentEstate
-                imgUrl="https://europetrippers.files.wordpress.com/2013/04/10597113.jpg"
-                name="Skb House"
-                location="Amsterdã, Holanda"
-                rooms={6}
-                bathrooms={4}
-                price="200.000"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <RecentEstate
-                imgUrl="https://news3lv.com/resources/media/710495dd-b841-4a4a-989b-d54c7483da3f-large3x4_A7R00137HDR.jpg?1564019073984"
-                name="Atisha Palace"
-                location="Las Vegas, Eua"
-                rooms={4}
-                bathrooms={3}
-                price="180.000"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <RecentEstate
-                imgUrl="https://i.pinimg.com/originals/93/f9/96/93f996eff6750f30d56f6fed11edd368.jpg"
-                name="S Palace"
-                location="Califórnia, Eua"
-                rooms={4}
-                bathrooms={2}
-                price="150.000"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <RecentEstate
-                imgUrl="https://www.caandesign.com/wp-content/uploads/2015/07/Al-Rio-de-Janeiro-02-1.jpg"
-                name="SA Villa"
-                location="Rio de Janeiro, Brasil"
-                rooms={3}
-                bathrooms={3}
-                price="120.000"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <RecentEstate
-                imgUrl="https://imagens-revista.vivadecora.com.br/uploads/2015/06/Reforma-de-fachada2.jpg"
-                name="A Place"
-                location="São Paulo, Brasil"
-                rooms={2}
-                bathrooms={3}
-                price="80.000"
-              />
-            </SwiperSlide>
+            {estates.map((estate) => (
+              <SwiperSlide>
+                <RecentEstate
+                  imgUrl={estate.img}
+                  name={estate.name}
+                  location={estate.location}
+                  rooms={estate.rooms}
+                  bathrooms={estate.bathrooms}
+                  price={estate.price}
+                />
+              </SwiperSlide>
+            ))}
           </Swiper>
         </section>
 
@@ -229,54 +182,16 @@ export default function App() {
           </p>
 
           <div className="houses container">
-            <RecentEstate
-              imgUrl="https://troyhomes.co.uk/wp-content/uploads/2016/11/Mirror-House-4.jpg"
-              name="Palace"
-              location="Toronto, Canadá"
-              rooms={5}
-              bathrooms={3}
-              price="240.000"
-            />
-            <RecentEstate
-              imgUrl="https://europetrippers.files.wordpress.com/2013/04/10597113.jpg"
-              name="Skb House"
-              location="Amsterdã, Holanda"
-              rooms={6}
-              bathrooms={4}
-              price="200.000"
-            />
-            <RecentEstate
-              imgUrl="https://news3lv.com/resources/media/710495dd-b841-4a4a-989b-d54c7483da3f-large3x4_A7R00137HDR.jpg?1564019073984"
-              name="Atisha Palace"
-              location="Las Vegas, Eua"
-              rooms={4}
-              bathrooms={3}
-              price="180.000"
-            />
-            <RecentEstate
-              imgUrl="https://i.pinimg.com/originals/93/f9/96/93f996eff6750f30d56f6fed11edd368.jpg"
-              name="S Palace"
-              location="Califórnia, Eua"
-              rooms={4}
-              bathrooms={2}
-              price="150.000"
-            />
-            <RecentEstate
-              imgUrl="https://www.caandesign.com/wp-content/uploads/2015/07/Al-Rio-de-Janeiro-02-1.jpg"
-              name="SA Villa"
-              location="Rio de Janeiro, Brasil"
-              rooms={3}
-              bathrooms={3}
-              price="120.000"
-            />
-            <RecentEstate
-              imgUrl="https://imagens-revista.vivadecora.com.br/uploads/2015/06/Reforma-de-fachada2.jpg"
-              name="A Place"
-              location="São Paulo, Brasil"
-              rooms={2}
-              bathrooms={3}
-              price="80.000"
-            />
+            {estates.map((estate) => (
+              <RecentEstate
+                imgUrl={estate.img}
+                name={estate.name}
+                location={estate.location}
+                rooms={estate.rooms}
+                bathrooms={estate.bathrooms}
+                price={estate.price}
+              />
+            ))}
           </div>
 
           <button type="button">Carregar mais</button>
