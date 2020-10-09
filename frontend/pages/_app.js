0021cf77d1b6fb81fa4e3f066cce5f43e330905c
lyandeveloperm <tutors3d@gmail.com>
2020-10-09 14:53:29 -0300

    refactor: Move global css to styled components

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,16 @@
-import "../global.css";
+import GlobalStyles from '../styles/Global';
 
-import "swiper/swiper.scss";
-import "swiper/components/navigation/navigation.scss";
-import "swiper/components/pagination/pagination.scss";
-import "swiper/components/scrollbar/scrollbar.scss";
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
 
-import "react-input-range/lib/css/index.css";
+import 'react-input-range/lib/css/index.css';
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ frontend/styles/Global.js
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: "Poppins", sans-serif;
+    }
+
+  button {
+    color: white;
+    font-weight: bold;
+    cursor: pointer;
+    background-color: #2289ff;
+    border-radius: 30px;
+    border: 0;
+    padding: 15px 20px;
+    transition: background-color 0.5s;
+  }
+
+  .button-secondary {
+    background: none;
+    color: #2289ff;
+    transition: color 0.5s;
+  }
+
+  .button-secondary:hover {
+    background: none;
+    color: #1c6ac4;
+  }
+
+  button:hover {
+    background-color: #1c6ac4;
+  }
+
+  .container {
+    max-width: 1280px;
+    padding: 0 2%;
+    margin: 0 auto;
+  }
+
+
+`;
+
+export default GlobalStyles;
