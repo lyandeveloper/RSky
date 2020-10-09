1f387bfa4d15ce3547ef269e275c9321f4eb9859
lyandeveloperm <tutors3d@gmail.com>
2020-10-09 14:45:00 -0300

    refactor: Move css to styled components

--- frontend/pages/explore.js
+++ frontend/pages/explore.js
@@ -1,10 +1,10 @@
-import React from "react";
-import Head from "next/head";
-import SwiperCore, { Navigation, Pagination, A11y, Autoplay } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import Header from "../components/HeaderSecondary";
-import SideBar from "../components/sideBar";
-import House from "../components/House";
+import React from 'react';
+import Head from 'next/head';
+import SwiperCore, { Navigation, Pagination, A11y, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Header from '../components/HeaderSecondary';
+import SideBar from '../components/SideBar';
+import House from '../components/House';
 
 SwiperCore.use([Navigation, Pagination, Autoplay, A11y]);
 
