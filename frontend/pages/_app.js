b019672b0010deb6bde7a5e251a5adf0dbd7d311
lyandeveloperm <tutors3d@gmail.com>
2020-10-09 21:28:13 -0300

    feat: Add loading bar

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,9 +1,17 @@
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
 import GlobalStyles from '../styles/Global';
 
 import 'swiper/swiper.scss';
 import 'swiper/components/navigation/navigation.scss';
 import 'swiper/components/pagination/pagination.scss';
 import 'swiper/components/scrollbar/scrollbar.scss';
+
+//Binding events.
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 import 'react-input-range/lib/css/index.css';
 export default function App({ Component, pageProps }) {
