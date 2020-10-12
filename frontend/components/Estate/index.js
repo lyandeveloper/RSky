e7d58e4b96573032bc2937efeceaaafbbfffa792
lyandeveloperm <tutors3d@gmail.com>
2020-10-12 01:35:16 -0300

    feat: Move style to styled components

--- frontend/components/Estate/index.js
+++ frontend/components/Estate/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
 import { FaRegHeart, FaBed, FaToilet, FaObjectUngroup } from 'react-icons/fa';
-// import { Container } from './styles';
+import {
+  EstateContainer,
+  EstateBanner,
+  EstateInfo,
+  EstateHeader,
+  EstatePrice,
+  EstateBody,
+  EstateBodyCategory,
+  EstateBodyDescription,
+  EstateBodyPlace,
+  EstateFooter,
+  EstateFooterFeatures,
+  EstateFooterItems,
+  EstateFooterInfo,
+  EstateFooterContact,
+  Button,
+} from './styles';
 
 function Estate({
   url,
@@ -16,111 +32,40 @@ function Estate({
 }) {
   return (
     <>
-      <a href={url} className="house-container">
-        <img src={imgUrl} alt="" />
-        <div className="house-info">
-          <div className="house-header">
-            <h2>{`R$ ${price}`}</h2>
+      <EstateContainer href={url}>
+        <EstateBanner src={imgUrl} alt="" />
+        <EstateInfo>
+          <EstateHeader>
+            <EstatePrice>{`R$ ${price}`}</EstatePrice>
             <FaRegHeart color="#2289ff" />
-          </div>
-          <div className="house-body">
-            <div className="house-body-category">{category}</div>
-            <div className="house-body-description">{description}</div>
-            <div className="house-body-place">{location}</div>
-          </div>
-          <div className="house-footer">
-            <div className="house-footer-features">
-              <div className="house-footer-items">
+          </EstateHeader>
+          <EstateBody>
+            <EstateBodyCategory>{category}</EstateBodyCategory>
+            <EstateBodyDescription>{description}</EstateBodyDescription>
+            <EstateBodyPlace>{location}</EstateBodyPlace>
+          </EstateBody>
+          <EstateFooter>
+            <EstateFooterFeatures>
+              <EstateFooterItems>
                 <FaObjectUngroup color="#2289ff" size={20} />
-                <span>{area}</span>
-              </div>
-              <div className="house-footer-items">
+                <EstateFooterInfo>{area}</EstateFooterInfo>
+              </EstateFooterItems>
+              <EstateFooterItems>
                 <FaToilet color="#2289ff" size={20} />
-                <span>{bathrooms}</span>
-              </div>
-              <div className="house-footer-items">
+                <EstateFooterInfo>{bathrooms}</EstateFooterInfo>
+              </EstateFooterItems>
+              <EstateFooterItems>
                 <FaBed color="#2289ff" size={20} />
-                <span>{rooms}</span>
-              </div>
-            </div>
-            <div className="house-footer-contact">
-              <button className="button-secondary">Telefone</button>
-              <button>Mensagem</button>
-            </div>
-          </div>
-        </div>
-      </a>
-
-      <style jsx>{`
-        .house-container {
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          background: #fff;
-          margin-top: 20px;
-          margin-left: 20px;
-          box-shadow: 3px 0px 8px 0px rgba(204, 204, 204, 1);
-          text-decoration: none;
-          color: #444;
-        }
-
-        .house-container img {
-          width: 400px;
-        }
-
-        .house-header {
-          display: flex;
-          flex-direction: row;
-          justify-content: space-between;
-          align-items: center;
-        }
-
-        .house-header > h2 {
-          color: #444;
-        }
-
-        .house-info {
-          margin-left: 20px;
-          padding: 0 20px;
-          width: 100%;
-        }
-
-        .house-body-description {
-          margin-top: 10px;
-          font-size: 14px;
-          font-weight: 300;
-        }
-
-        .house-body-place {
-          margin-top: 10px;
-          font-size: 14px;
-          font-weight: 300;
-        }
-
-        .house-footer {
-          margin-top: 20px;
-          display: flex;
-          flex-direction: row;
-          justify-content: space-between;
-          align-items: center;
-        }
-
-        .house-footer-features {
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-        }
-
-        .house-footer-items {
-          margin-right: 10px;
-          display: flex;
-          align-items: center;
-        }
-
-        .house-footer-items span {
-          margin-left: 5px;
-        }
-      `}</style>
+                <EstateFooterInfo>{rooms}</EstateFooterInfo>
+              </EstateFooterItems>
+            </EstateFooterFeatures>
+            <EstateFooterContact>
+              <Button className="button-secondary">Telefone</Button>
+              <Button>Mensagem</Button>
+            </EstateFooterContact>
+          </EstateFooter>
+        </EstateInfo>
+      </EstateContainer>
     </>
   );
 }
