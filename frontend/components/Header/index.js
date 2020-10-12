3be3e185f69e36e1842994d09f4c677dafaac5c7
lyandeveloperm <tutors3d@gmail.com>
2020-10-12 01:56:46 -0300

    refactor: Header move style to styled components

--- frontend/components/Header/index.js
+++ frontend/components/Header/index.js
@@ -1,63 +1,37 @@
-import React from "react";
-import Link from "next/link";
-// import { Container } from './styles';
+import React from 'react';
+import Link from 'next/link';
+import {
+  HeaderContainer,
+  Logo,
+  LogoDetail,
+  Menu,
+  MenuContent,
+  MenuItem,
+} from './styles';
 
 function Header() {
   return (
-    <header className="container">
-      <div className="logo">
-        <span>RS</span>ky
-      </div>
-      <nav>
-        <ul>
+    <HeaderContainer className="container">
+      <Logo href="/">
+        <LogoDetail>RS</LogoDetail>ky
+      </Logo>
+      <Menu>
+        <MenuContent>
           <Link href="#">
-            <li>Home</li>
+            <MenuItem>Home</MenuItem>
           </Link>
           <Link href="/explore">
-            <li>Explorar</li>
+            <MenuItem>Explorar</MenuItem>
           </Link>
           <Link href="#">
-            <li>Sobre</li>
+            <MenuItem>Sobre</MenuItem>
           </Link>
           <Link href="#">
-            <li>Contato</li>
+            <MenuItem>Contato</MenuItem>
           </Link>
-        </ul>
-      </nav>
-      <button>Cadastre-se</button>
-      <style jsx>
-        {`
-          header {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            flex-direction: row;
-            padding: 10px 0; 
-          }
-          header .logo {
-            font-weight: bold;
-            font-size: 28px;
-            color: #888;
-          }
-
-          .logo span {
-            color: #2289FF;
-          }
-          ul {
-            display: flex;
-            align-items: center:
-            flex-direction: row;
-            list-style: none;
-          }
-
-          li {
-            margin-right: 20px;
-            color: #193f6a;
-            cursor: pointer;
-          } 
-        `}
-      </style>
-    </header>
+        </MenuContent>
+      </Menu>
+    </HeaderContainer>
   );
 }
 
---
+++ frontend/components/Header/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import { MAIN, SECONDARY, GRAY } from '../../styles/Colors';
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  padding: 10px 0;
+`;
+
+export const Logo = styled.a`
+  font-weight: bold;
+  font-size: 28px;
+  color: ${GRAY};
+  text-decoration: none;
+`;
+
+export const LogoDetail = styled.span`
+  color: ${MAIN};
+`;
+
+export const Menu = styled.nav``;
+
+export const MenuContent = styled.ul`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  list-style: none;
+`;
+
+export const MenuItem = styled.li`
+  margin-right: 20px;
+  color: ${SECONDARY};
+  cursor: pointer;
+`;
