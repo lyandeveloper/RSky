a1a3bfab5edce6a62571c1e0110bcfed3384d7f4
lyandeveloper <tutors3d@gmail.com>
2021-04-14 11:11:30 -0300

    refactor: removed unsable backend

--- backend/src/repositories/IEstatesRepository.ts
+++
@@ -1,6 +0,0 @@
-import { Estate } from '../entities/Estate';
-
-export interface IEstatesRepository {
-  createEstate(estate: Estate): Promise<void>;
-  findEstateByIdAndSlug(id: string, slug: string): Promise<Estate>;
-}
