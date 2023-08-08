dce1b66d8ab4074f5f95c3ec7b52da59d4ff7d22
lyandeveloper <tutors3d@gmail.com>
2023-08-07 22:42:40 -0300

    refactor: use sass instead of styled components in sidebar component

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,28 +1,16 @@
 import React from 'react';
 import InputRange from 'react-input-range';
 import { useState } from 'react';
-import {
-  SidebarContainer,
-  Section,
-  SectionTitle,
-  Input,
-  InputDetail,
-  Label,
-  InputContainer,
-  Select,
-  Option,
-  FeaturesWrapper,
-  FeaturesInput,
-} from './styles';
+import styles from './styles.module.scss';
 
 function sideBar() {
   const [value, setValue] = useState<any>(150000);
   return (
     <>
-      <SidebarContainer>
-        <Section>
-          <SectionTitle>Preço</SectionTitle>
-          <InputContainer>
+      <aside className={styles.aside}>
+        <section>
+          <h2>Preço</h2>
+          <div className={styles.input_container}>
             <InputRange
               value={value}
               maxValue={150000}
@@ -32,101 +20,101 @@ function sideBar() {
               name="Preço"
               step={3}
             />
-          </InputContainer>
-        </Section>
-        <Section>
-          <SectionTitle>Status</SectionTitle>
-          <InputContainer>
-            <Input
+          </div>
+        </section>
+        <section>
+          <h2>Status</h2>
+          <div className={styles.input_container}>
+            <input
               type="radio"
               name="status"
               id=""
               value="new"
-              checked="checked"
+              checked
             />
 
-            <Label>Nova</Label>
-          </InputContainer>
-          <InputContainer>
-            <Input type="radio" name="status" value="used" id="" />
-            <Label>Usada</Label>
-          </InputContainer>
-        </Section>
+            <label>Nova</label>
+          </div>
+          <div className={styles.input_container}>
+            <input type="radio" name="status" value="used" id="" />
+            <label>Usada</label>
+          </div>
+        </section>
 
-        <Section>
-          <SectionTitle>Categoria</SectionTitle>
-          <Select name="category" id="">
-            <Option value="">Todos os imóveis</Option>
-            <Option value="">Apartamento</Option>
-            <Option value="">Casa</Option>
-            <Option value="">Casa de condomínio</Option>
-            <Option value="">Cobertura</Option>
-            <Option value="">Flat</Option>
-            <Option value="">Loft</Option>
-            <Option value="">Terreno / Lote / Condomínio</Option>
-          </Select>
-        </Section>
+        <section>
+          <h2>Categoria</h2>
+          <select name="category" id="">
+            <option value="">Todos os imóveis</option>
+            <option value="">Apartamento</option>
+            <option value="">Casa</option>
+            <option value="">Casa de condomínio</option>
+            <option value="">Cobertura</option>
+            <option value="">Flat</option>
+            <option value="">Loft</option>
+            <option value="">Terreno / Lote / Condomínio</option>
+          </select>
+        </section>
 
-        <Section>
-          <SectionTitle>Banheiros</SectionTitle>
-          <Select name="" id="">
-            <Option value="">1</Option>
-            <Option value="">2</Option>
-            <Option value="">3</Option>
-            <Option value="">4</Option>
-          </Select>
-        </Section>
+        <section>
+          <h2>Banheiros</h2>
+          <select name="" id="">
+            <option value="">1</option>
+            <option value="">2</option>
+            <option value="">3</option>
+            <option value="">4</option>
+          </select>
+        </section>
 
-        <Section>
-          <SectionTitle>Quartos</SectionTitle>
-          <Select name="" id="">
-            <Option value="">1</Option>
-            <Option value="">2</Option>
-            <Option value="">3</Option>
-            <Option value="">4</Option>
-            <Option value="">5</Option>
-          </Select>
-        </Section>
+        <section>
+          <h2>Quartos</h2>
+          <select name="" id="">
+            <option value="">1</option>
+            <option value="">2</option>
+            <option value="">3</option>
+            <option value="">4</option>
+            <option value="">5</option>
+          </select>
+        </section>
 
-        <Section>
-          <SectionTitle>Recursos</SectionTitle>
-          <FeaturesWrapper>
-            <FeaturesInput>
-              <InputDetail>
-                <Input type="checkbox" name="garden" id="" value="garden" />
-                <Label>Jardim</Label>
-              </InputDetail>
+        <section>
+          <h2>Recursos</h2>
+          <div className={styles.features_wrapper}>
+            <div className={styles.features_input}>
+              <div className={styles.input_details}>
+                <input type="checkbox" name="garden" id="" value="garden" />
+                <label>Jardim</label>
+              </div>
 
-              <InputDetail>
-                <Input type="checkbox" name="garage" id="" value="garage" />
-                <Label>Garagem</Label>
-              </InputDetail>
+              <div className={styles.input_details}>
+                <input type="checkbox" name="garage" id="" value="garage" />
+                <label>Garagem</label>
+              </div>
 
-              <InputDetail>
-                <Input type="checkbox" name="balcony" id="" value="balcony" />
-                <Label>Sacada</Label>
-              </InputDetail>
-            </FeaturesInput>
+              <div className={styles.input_details}>
+                <input type="checkbox" name="balcony" id="" value="balcony" />
+                <label>Sacada</label>
+              </div>
+            </div>
 
-            <FeaturesInput>
-              <InputDetail>
-                <Input type="checkbox" name="air" id="" value="air" />
-                <Label>Ar </Label>
-              </InputDetail>
+            <div className={styles.features_input}>
+              <div className={styles.input_details}>
+                <input type="checkbox" name="air" id="" value="air" />
+                <label>Ar </label>
+              </div>
 
-              <InputDetail>
-                <Input type="checkbox" name="laundry" id="" value="laundry" />
-                <Label>Lavanderia</Label>
-              </InputDetail>
+              <div className={styles.input_details}>
+                <input type="checkbox" name="laundry" id="" value="laundry" />
+                <label>Lavanderia</label>
+              </div>
 
-              <InputDetail>
-                <Input type="checkbox" name="backwayd" id="" value="backyard" />
-                <Label>Quintal</Label>
-              </InputDetail>
-            </FeaturesInput>
-          </FeaturesWrapper>
-        </Section>
-      </SidebarContainer>
+              <div className={styles.input_details}>
+                <input type="checkbox" name="backwayd" id="" value="backyard" />
+                <label>Quintal</label>
+              </div>
+            </div>
+          </div>
+        </section>
+      </aside>
     </>
   );
 }
