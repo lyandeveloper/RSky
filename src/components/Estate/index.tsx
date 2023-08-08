d5f7dc9f64fc2e30e454af0992c066ad9d287fce
lyandeveloper <tutors3d@gmail.com>
2023-08-07 21:56:05 -0300

    refactor: use sass instead of styled components in estate component

--- src/components/Estate/index.tsx
+++ src/components/Estate/index.tsx
@@ -1,21 +1,8 @@
 import CurrencyFormat from 'react-currency-format';
 import { FaBed, FaObjectUngroup, FaToilet } from 'react-icons/fa';
 import { MAIN } from '../../styles/Colors';
-import {
-  EstateBanner,
-  EstateBody,
-  EstateBodyCategory,
-  EstateBodyDescription,
-  EstateBodyPlace,
-  EstateContainer,
-  EstateFooter,
-  EstateFooterFeatures,
-  EstateFooterInfo,
-  EstateFooterItems,
-  EstateHeader,
-  EstateInfo,
-  EstatePrice
-} from './styles';
+import styles from './styles.module.scss';
+import estates from '../../services/content';
 
 type EstateProps = {
   url: string;
@@ -42,42 +29,42 @@ function Estate({
 }: EstateProps) {
   return (
     <>
-      <EstateContainer href={url}>
-        <EstateBanner src={imgUrl} alt="" />
-        <EstateInfo>
-          <EstateHeader>
-            <EstatePrice>
+      <a className={styles.estate_container} href={url}>
+        <img src={imgUrl} alt="" />
+        <div className={styles.estate_info}>
+          <div className={styles.estate_header}>
+            <h2>
               <CurrencyFormat
                 value={price}
                 displayType={'text'}
                 thousandSeparator={true}
                 prefix={'R$'}
               />{' '}
-            </EstatePrice>
-          </EstateHeader>
-          <EstateBody>
-            <EstateBodyCategory>{category}</EstateBodyCategory>
-            <EstateBodyDescription>{description}</EstateBodyDescription>
-            <EstateBodyPlace>{location}</EstateBodyPlace>
-          </EstateBody>
-          <EstateFooter>
-            <EstateFooterFeatures>
-              <EstateFooterItems>
+            </h2>
+          </div>
+          <div className={styles.estate_body}>
+            <span>{category}</span>
+            <p className={styles.estate_body_description}>{description}</p>
+            <span className={styles.estate_body_place}>{location}</span>
+          </div>
+          <div className={styles.estate_footer}>
+            <div className={styles.estate_footer_features}>
+              <div className={styles.estate_footer_items}>
                 <FaObjectUngroup color={MAIN} size={20} />
-                <EstateFooterInfo>{area}</EstateFooterInfo>
-              </EstateFooterItems>
-              <EstateFooterItems>
+                <span>{area}</span>
+              </div>
+              <div className={styles.estate_footer_items}>
                 <FaToilet color={MAIN} size={20} />
-                <EstateFooterInfo>{bathrooms}</EstateFooterInfo>
-              </EstateFooterItems>
-              <EstateFooterItems>
+                <span>{bathrooms}</span>
+              </div>
+              <div className={styles.estate_footer_items}>
                 <FaBed color={MAIN} size={20} />
-                <EstateFooterInfo>{rooms}</EstateFooterInfo>
-              </EstateFooterItems>
-            </EstateFooterFeatures>
-          </EstateFooter>
-        </EstateInfo>
-      </EstateContainer>
+                <span>{rooms}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
     </>
   );
 }
